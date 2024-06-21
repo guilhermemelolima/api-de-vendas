84c5f6045a4b01a56536ea583c1ed49416a59b77
Guilherme Melo de Lima <limaguilherme.melo@gmail.com>
2024-06-20 21:12:01 -0300

    add findByName no TransportadoraRepository

--- src/main/java/com/atividade/apidevendas/domain/transportadora/TransportadoraRepository.java
+++ src/main/java/com/atividade/apidevendas/domain/transportadora/TransportadoraRepository.java
@@ -2,8 +2,11 @@ package com.atividade.apidevendas.domain.transportadora;
 
 import com.atividade.apidevendas.entities.Transportadora;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TransportadoraRepository extends JpaRepository<Transportadora, Long> {
+    @Query("SELECT t FROM Transportadora t WHERE LOWER(t.nome) LIKE LOWER(CONCAT('%', :nome, '%'))")
+    List<Transportadora> findByName(String nome);
 }
